50eca4f0ea40946d7240e35800462898e53c3fa6
Agaba Kabunga <agaba_k@hotmail.com>
2019-04-09 15:54:12 -0500

    add login functinality

--- Datingapp-SPA/src/app/app.module.ts
+++ Datingapp-SPA/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
